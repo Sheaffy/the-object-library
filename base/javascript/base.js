d51eea7337aca1a91342f8e01d247e6862cc6063
Sheaffy <sheaffy.ashley@gmail.com>
2022-07-27 20:15:56 +0100

    testing ideas

---
+++ base/javascript/base.js
@@ -0,0 +1,10 @@
+class OL_Base {
+    constructor(options){
+        this.fill(options)
+    }
+
+    fill(options){
+        let keys = Object.keys(this)
+        console.log(keys)
+    }
+}
---
+++ person/javascript/person.js
@@ -0,0 +1,12 @@
+class Person extends OL_Base {
+
+    firstName = "";
+    lastName = "";
+    dateOfBirth = null;
+
+    constructor(options)
+    {
+        super(options)
+    }
+
+}
